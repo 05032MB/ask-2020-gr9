cef874014c237c57c4cc46cca90023d49348f3b8
05032MB <36550385+05032MB@users.noreply.github.com>
2020-06-29 17:02:03 +0200

    Command line args

--- turt.py
+++ turt.py
@@ -2,6 +2,7 @@ from turtle import *
 from serial import *
 from time import sleep
 import os
+import sys
 
 def get_usb_device():
     usb_devices = list(filter(lambda d : 'usb' in d.lower(), os.listdir('/dev/')))
@@ -48,21 +49,48 @@ def toggle_write():
     else:
         turt.down()
 
+def send_char(char : str):
+    print('Diode flipped')
+    ser.write(char.encode('utf-8'))
+
 #def back():
 #    turt.back(45)
 
-onkey(bye, "q")
+def manual():
+    onkey(bye, "q")
+    onkey(onward, "Up")
+    onkey(rot_left, "Left")
+    onkey(rot_right, "Right")
+    onkey(toggle_write, 'u')
+    onkey(turt.down, 'D')
+    onkey(turt.up, 'U')
+    onkey(lambda : send_char('9'), 'x')
+    #onkey(back, "Down")
+    listen()
+    mainloop()
 
-sleep(5)
-print("Robot gotowy")
+def from_args(command_list):
+    commands = {
+        'u' : turt.up,
+        'd' : turt.down,
+        'f' : onward,
+        'l' : rot_left,
+        'r' : rot_right,
+        't' : toggle_write
+    }
+    for command in command_list:
+        try:
+            commands[command]()
+        except Exception as e:
+            print(e)
+            break
+    else:
+        print("Command list executed")
 
-onkey(onward, "Up")
-onkey(rot_left, "Left")
-onkey(rot_right, "Right")
-onkey(toggle_write, 'u')
-onkey(lambda : turt.down(), 'D')
-onkey(lambda : turt.up(), 'U')
-#onkey(back, "Down")
-
-listen()
-mainloop()
+if __name__ == '__main__':
+    sleep(5)
+    print("Robot gotowy")
+    if len(sys.argv) == 1:
+        manual()
+    elif len(sys.argv) > 1:
+        from_args(sys.argv[1])
